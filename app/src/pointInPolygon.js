d6a7662070b07910193572a3531ec0d5aac1c476
Chartoff <bchartoff@urban.org>
2015-01-20 12:35:55 -0500

    removed comments/ console.logs

--- app/src/pointInPolygon.js
+++ app/src/pointInPolygon.js
@@ -1,23 +1,12 @@
-
 var fs = require("fs");
 var czones = JSON.parse(fs.readFileSync("../json/czone.geo.json", "utf8"));
 var cities = JSON.parse(fs.readFileSync("../json/cities.json", "utf8"));
 var wherewolf = require('wherewolf');
 var wolf = new wherewolf()
-// console.log(w)
-// data = JSON.parse(data)
-// console.log(data)
-//Load the data asynchronously
-// wolf = Wherewolf();
-// console.log(data)
-// wolf.add("czones",data);
-// for (var name in data.objects.czones) {
-//   wolf.add(name,data,name);
-//   console.log("foo")
-// }
+
 wolf.add("czone",czones,"czones")
 var output = {"type": "FeatureCollection","features":[]}
-// console.log(wolf.find([-71.057756783011428,42.358847171748607],{ wholeFeature: true }))
+
 for(var i = 0; i < cities.features.length; i++) {
     var obj = cities.features[i];
     var coords = obj.geometry.coordinates;
